fc8de559d73c9c591d03405173ac267cc9dfeb1b
Isupov Maxim <maksa2901@yandex.ru>
2022-02-05 17:12:28 +0300

    Добавил Postgres контейнером

--- code/insurance_django_project/settings.py
+++ code/insurance_django_project/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'insurance_django_project.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": os.environ.get("POSTGRES_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("POSTGRES_DB", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER": os.environ.get("POSTGRES_USER", "user"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "password"),
+        "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
+        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
     }
 }
 
